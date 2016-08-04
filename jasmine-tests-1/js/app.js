d1b01fbd65e776ff0a0835c7f6428c6f8e457a38
jerfolsi <jerfolsi@gmail.com>
2016-08-04 15:53:44 +0200

    Add files via upload

---
+++ jasmine-tests-1/js/app.js
@@ -0,0 +1,17 @@
+angular.module('app', [])
+  .controller('PasswordController', function($scope) {
+
+    $scope.password = '';
+
+    $scope.grade = function() {
+        var size = $scope.password.length;
+        if (size > 8) {
+          $scope.strength = 'strong';
+        } else if (size > 3) {
+          $scope.strength = 'medium';
+        } else {
+          $scope.strength = 'weak';
+        }
+      };
+
+  });
